fe5b1187e4dffb740e39861768d1d2fbc47f85d8
Mario Garza <mario.garza02@utrgv.edu>
2020-04-24 04:07:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  ROSTER={}
+  attr_reader :name,:roster
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  def add_student(name,grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+      else
+        roster[grade]=[name]
+    end
+  end
+  
+  def grade(grade)
+    
+
+  end
+  
+  
+end
